2ebebb1073b2752f6e69f83c38aebdaf6fbf0a5b
LOGANATHAN9318 <loganathan9318@gmail.com>
2022-03-07 18:12:38 +0000

    Commit Successful

--- Airlines_Performance_Quries/Airlines_Performance_Quries_1_loganathan.sql
+++ Airlines_Performance_Quries/Airlines_Performance_Quries_1_loganathan.sql
@@ -1,6 +1,6 @@
 -->Performance Quries Set 1 
--->1.Count the female passengers book an air ticket
 
+-->1.Count the female passengers book an air ticket
 CREATE OR REPLACE FUNCTION COUNT_FEMALE_PASSENGER
 RETURN VARCHAR
 IS
@@ -35,8 +35,72 @@ FROM
 WHERE 
     CLASS = 'Business';
 
--->4.
+-->4. Count the kid passenger list? (kid = age is less than 13)
+SELECT
+    COUNT(AGE) "Kid Passenger"
+FROM
+    AIRLINE
+WHERE
+    AGE < 13;
 
+-->5. Count the passenger list of each Travel type
+SELECT
+    TYPE_OF_TRAVEL,
+    COUNT(1) "Passenger Count"
+FROM
+    AIRLINE
+GROUP BY
+    TYPE_OF_TRAVEL;
+
+-->6. Count of passengers booking eco class
+SELECT
+    CLASS,
+    COUNT(1) "Passenger Count"
+FROM
+    AIRLINE
+WHERE
+    CLASS = 'Eco'
+GROUP BY
+    CLASS;
+
+-->7. Count the passenger list above age 16 and book a business class
+SELECT
+    COUNT(1) "Passenger Count Age > 16 and Business class"
+FROM
+    AIRLINE
+WHERE
+    AGE > 16 AND CLASS = 'Business';
+
+-->8. Count the passenger list of each satisfaction
+SELECT
+    SATISFACTION,
+    COUNT(1) "Passenger Count"
+FROM
+    AIRLINE
+GROUP BY
+    SATISFACTION;    
+
+-->9. Display the customer type where gender is male and display them according to the class
+SELECT
+    CUSTOMER_TYPE,
+    CLASS
+FROM
+    AIRLINE
+WHERE 
+    GENDER = 'Male'
+ORDER BY
+    CLASS DESC;
+
+
+-->10. Count the passenger list of each satisfaction? 
+
+SELECT
+    SATISFACTION,
+    COUNT(1) "Passenger Count"
+FROM
+    AIRLINE
+GROUP BY
+    SATISFACTION;    
 
 --Reference Quries
 SELECT 
@@ -48,6 +112,14 @@ SELECT
     * 
 FROM 
     AIRLINE;
+
+
+SELECT
+    *
+FROM
+    AIRLINE
+WHERE
+    GENDER = 'Male';
 
 DESC AIRLINE;
 
