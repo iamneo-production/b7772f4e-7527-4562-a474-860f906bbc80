ae7287d42dd8345607f9b0438926237f072013b6
LOGANATHAN9318 <loganathan9318@gmail.com>
2022-03-07 11:32:05 +0000

    Commit Successful

--- Airlines_Performance_Quries/Airlines_Performance_Quries_1_loganathan.sql
+++ Airlines_Performance_Quries/Airlines_Performance_Quries_1_loganathan.sql
@@ -1,15 +1,55 @@
+-->Performance Quries Set 1 
+
+-->1.Count the female passengers book an air ticket
+
 CREATE OR REPLACE FUNCTION COUNT_FEMALE_PASSENGER
 RETURN VARCHAR
 IS
-FEMALE_COUNT NUMBER := 0;
+Female_Count NUMBER := 0;
 BEGIN
-SELECT COUNT(1) INTO FEMALE_COUNT FROM AIRLINE WHERE GENDER = 'Female';
-RETURN 'TOTAL FEMALE PASSENGERS '||FEMALE_COUNT;
+    SELECT 
+        COUNT(1) 
+    INTO 
+        Female_Count
+    FROM 
+        AIRLINE 
+    WHERE 
+        GENDER = 'Female';
+    RETURN 'TOTAL FEMALE PASSENGERS '||Female_Count;
 END;
 /
 
-SELECT CLASS,COUNT(1) FROM AIRLINE GROUP BY CLASS;
+-->2. Count the passenger list class-wise
+SELECT 
+    CLASS "Travel Class", 
+    COUNT(1) "Passenger Count" 
+FROM 
+    AIRLINE 
+GROUP BY 
+    CLASS;
+
+-->3.The passenger count who is all booking business Class
+SELECT 
+    COUNT(1) "Booking Count of Business Class" 
+FROM 
+    AIRLINE 
+WHERE 
+    CLASS = 'Business';
+
+-->4.
 
 
+--Reference Quries
+SELECT 
+    COUNT_FEMALE_PASSENGER() 
+FROM 
+    DUAL;
 
-SELECT COUNT_FEMALE_PASSENGER() FROM DUAL;
+SELECT 
+    * 
+FROM 
+    AIRLINE;
+
+DESC AIRLINE;
+
+--To view all Indexies from Table AIRLINE
