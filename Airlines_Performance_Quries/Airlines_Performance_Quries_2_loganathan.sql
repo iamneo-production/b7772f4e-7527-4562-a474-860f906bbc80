70560e1f69ddc4d6a69c8f8618322c672d078b39
LOGANATHAN9318 <loganathan9318@gmail.com>
2022-03-08 13:18:10 +0000

    Commit Successful

--- Airlines_Performance_Quries/Airlines_Performance_Quries_2_loganathan.sql
+++ Airlines_Performance_Quries/Airlines_Performance_Quries_2_loganathan.sql
@@ -20,4 +20,37 @@ GROUP BY
     CLASS;
 
 -->3. Find The Passengers who are all booking a ticket age above 15 in Business class
+SELECT 
+    ID
+FROM
+    AIRLINE
+WHERE
+    CLASS = 'Business' AND AGE > 15;
+
+-->4. Count the Passengers in each Customer Type
 SELECT
+    CUSTOMER_TYPE,
+    COUNT(1) "Total Passengers"
+FROM
+    AIRLINE
+GROUP BY
+    CUSTOMER_TYPE;
+
+-->5. Display the passenger details who are all booking Eco class
+SELECT
+    ID
+FROM
+    AIRLINE
+WHERE
+    CLASS ='Eco';
+
+
+SELECT ST_NAME,SUM(COUNT(ST_NAME)),ROUND(SUM(TOTVOTPOLL)/SUM(ELECTORS)*100,2) FROM ELECTION WHERE PARTYABBRE = 'BJP'
+GROUP BY ST_NAME HAVING ROUND(SUM(TOTVOTPOLL)/SUM(ELECTORS)*100,2) > 20;
+
+SELECT * FROM ELECTION;
+
+SELECT YEAR,TOTVOTPOLL,ELECTORS,PARTYABBRE FROM ELECTION WHERE PARTYABBRE ='BJP' 
+GROUP BY YEAR,TOTVOTPOLL,ELECTORS,PARTYABBRE;
+
+SELECT 
